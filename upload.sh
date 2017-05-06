c5977770be908ae30cc6b829c28d75e82f8c858f
alfred <57082212@qq.com>
2017-05-06 15:37:10 +0800

    add upload message

--- upload.sh
+++ upload.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
+if [ ! -e ~/.pypirc ]; then
+cat <<EOF
+# You haven't configure ~/.pypirc config file.
+# In order to upload your package to PyPI, You need to configure first:
+
+# Example:
+
+cat <<PYPI >~/.pypirc
+
+[distutils]
+index-servers =
+    pypi
+    pypitest
+
+[pypi]
+username:fish_ball
+password:******
+
+[pypitest]
+repository: https://testpypi.python.org/pypi
+username:fish_ball
+password:******
+
+PYPI
+
+EOF
+fi
+  
+
 python setup.py sdist upload -r pypi
